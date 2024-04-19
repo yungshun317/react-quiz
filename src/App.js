75b24be157ee490dbcbbdbb865a180720273b3a1
yungshun317 <seraph317@gmail.com>
2024-04-19 22:01:58 +0800

    [FEAT] Update `questions` and `status` with the `dispatch` function returned by `useReducer`

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import DateCounter from "./DateCounter";
 import Header from "./Header";
 import Main from "./Main";
-import {useEffect} from "react";
+import {useEffect, useReducer} from "react";
+
+const initialState = {
+    questions: [],
+
+    // 'loading', 'error', 'ready', 'active', 'finished'
+    status: "loading"
+};
+
+function reducer(state, action) {
+    switch(action.type) {
+        case "dataReceived":
+            return {
+                ...state,
+                question: action.payload
+            }
+        default:
+            throw new Error("Action unknown");
+    }
+}
 
 export default function App() {
+    const [stata, dispatch] = useReducer(reducer, initialState);
+
     useEffect(function() {
         fetch("http://localhost:8000/questions")
             .then((res) => res.json())
-            .then((data) => console.log(data))
+            .then((data) => dispatch({ type: "dataReceived", payload: data }))
             .catch((err) => console.error("Error"));
     }, []);
     /*
