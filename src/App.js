ec07c4dcdb24d820431e72c8d30dd4f41013cde8
yungshun317 <seraph317@gmail.com>
2024-04-20 08:56:36 +0800

    [FEAT] Start a new quiz

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {useEffect, useReducer} from "react";
 import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
+import Question from "./Question";
 
 const initialState = {
     questions: [],
@@ -26,6 +27,8 @@ function reducer(state, action) {
                 ...state,
                 status: "error"
             }
+        case "start":
+            return { ...state, status: "active" }
         default:
             throw new Error("Action unknown");
     }
@@ -70,7 +73,8 @@ export default function App() {
             <Main>
                 {status === "loading" && <Loader />}
                 {status === "error" && <Error />}
-                {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+                {status === "ready" && <StartScreen numQuestions={numQuestions} dispatch={dispatch} />}
+                {status === "active" && <Question />}
             </Main>
         </div>
     )
