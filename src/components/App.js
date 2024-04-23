5e6829599f13f5aafa2e58c352e928ee01b8ffe7
yungshun317 <seraph317@gmail.com>
2024-04-24 06:23:34 +0800

    [FEAT] Use `clearInterval` to stop the timer in the `useEffect` cleanup function and calculate the amount of seconds based on the number of questions

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,8 @@ import FinishScreen from "./FinishScreen";
 import Footer from "./Footer";
 import Timer from "./Timer";
 
+const SECS_PER_QUESTION = 30;
+
 const initialState = {
     questions: [],
 
@@ -21,8 +23,7 @@ const initialState = {
     answer: null,
     points: 0,
     highscore: 0,
-    secondsRemaining: 10
-
+    secondsRemaining: null
 };
 
 function reducer(state, action) {
@@ -39,7 +40,7 @@ function reducer(state, action) {
                 status: "error"
             }
         case "start":
-            return { ...state, status: "active" }
+            return { ...state, status: "active", secondsRemaining: state.questions.length * SECS_PER_QUESTION }
         case "newAnswer":
             const question = state.questions.at(state.index);
 
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,14 +1,21 @@
 import { useEffect } from "react";
 
 function Timer({ dispatch, secondsRemaining }) {
+    const mins = Math.floor(secondsRemaining / 60);
+    const seconds = secondsRemaining % 60;
+
     useEffect(function() {
-        setInterval(function() {
-            console.log("tick");
+        const id = setInterval(function() {
+            // console.log("tick");
             dispatch({ type: "tick" })
         }, 1000);
+
+        return () => clearInterval(id);
     }, [dispatch]);
 
-    return <div className="timer">{secondsRemaining}</div>;
+    return (
+        <div className="timer">{mins < 10 && "0"}{mins}:{seconds < 10 && "0"}{seconds}</div>
+    );
 }
 
 export default Timer;
